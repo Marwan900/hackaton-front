a2b85dfe4525a85a607f544f023de9bfb0562cf4
Marwan900 <marwan.elfakih@gmail.com>
2023-07-04 16:19:27 +0200

    Ajout page Settings

--- Screen/Notification.js
+++ Screen/Notification.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-
 const NotificationPage = () => {
+  const notifications = [
+    { type: 'chat', message: 'Nouveau message de John' },
+    { type: 'post', message: 'Nouvelle publication de Jane à proximité' },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Notifications</Text>
-      <Text style={styles.notification}>Notification 1</Text>
-      <Text style={styles.notification}>Notification 2</Text>
-      <Text style={styles.notification}>Notification 3</Text>
-      {/* Ajoutez ici le contenu de vos notifications */}
+      <Text style={styles.title}>Mes Notifications</Text>
+      {notifications.map((notification, index) => (
+        <View key={index} style={styles.notificationContainer}>
+          {notification.type === 'chat' ? (
+            <Text style={styles.chatNotification}>{notification.message}</Text>
+          ) : (
+            <Text style={styles.postNotification}>{notification.message}</Text>
+          )}
+        </View>
+      ))}
     </View>
   );
 };
@@ -18,7 +27,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#F2F1EC',
-    justifyContent: 'center',
     alignItems: 'center',
   },
   title: {
@@ -26,9 +34,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
   },
-  notification: {
-    fontSize: 18,
+  notificationContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 10,
     marginBottom: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    width: '80%',
+  },
+  chatNotification: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  postNotification: {
+    fontSize: 18,
   },
 });
 
